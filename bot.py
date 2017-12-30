f3036e52461082b6bd1d957171de73fa9da4ca78
Chase Ufkes <jufkes@gmail.com>
2017-12-30 10:58:05 -0600

    Fixed order placement to rebase on last order

--- bot.py
+++ bot.py
@@ -60,6 +60,16 @@ def get_last_order(history):
     print (data)
     print (data[0])
 
+def get_last_buy_order(completedBuyOrders):
+    data = completedBuyOrders['datas']
+    orderTime = (data[0]['createdAt'])
+    return(orderTime)
+
+def get_last_sell_order(completedSellOrders):
+    data = completedSellOrders['datas']
+    orderTime = (data[0]['createdAt'])
+    return (orderTime)
+
 def post_slack(type):
     logging.info("Attempting to send message...")
     sc = SlackClient(token)
@@ -70,14 +80,35 @@ def post_slack(type):
         text=text
     )
 
+def get_order_price(orderData):
+    data = orderData['datas']
+    data = data[0]
+    price = data['dealPrice']
+    return price
+
+def isWithinChecktime(timeToCheckInMillis, baseTimeInMillis, intervalInSec):
+    return abs(timeToCheckInMillis - baseTimeInMillis) <= (intervalInSec * 1000)
+
+def determine_order_data(data):
+    try:
+        logging.info("Found order: " + str(data[0]))
+        order = True
+    except:
+        order = False
+    return order
+
+
 
 cycle = 0
 while True:
     try:
         openOrders = (client.get_active_orders(tokenPair))
+        sellOrderData = openOrders['SELL']
+        buyOrderData = openOrders['BUY']
         if (openOrders['BUY']) and (openOrders['SELL']):
             logging.info(openOrders)
-        else:
+        elif ((determine_order_data(sellOrderData) == False) and (determine_order_data(buyOrderData) == True))\
+                or ((determine_order_data(sellOrderData) == True) and (determine_order_data(buyOrderData) == False)):
             for type in openOrders:
                 try:
                     oid = get_oid(openOrders[type])
@@ -87,17 +118,43 @@ while True:
                     logging.info ("Order type " + type + " completed")
                     if token:
                         post_slack(type)
+            completedSellOrders = client.get_deal_orders(tokenPair, 'SELL')
+            completedBuyOrders = client.get_deal_orders(tokenPair, 'BUY')
+            checkTime = time.time() * 1000
+            logging.info("BaseTime: " + str(checkTime))
+            if (isWithinChecktime(get_last_buy_order(completedBuyOrders), checkTime, checkInterval)):
+                logging.info("Order executed within " + str(checkInterval) + "...getting price")
+                price = (get_order_price(completedBuyOrders))
+                logging.info("Last buy price: " + str(price))
+            if (isWithinChecktime(get_last_sell_order(completedSellOrders), checkTime, checkInterval)):
+                logging.info("Order executed within " + str(checkInterval) + "...getting price")
+                price = (get_order_price(completedSellOrders))
+                logging.info("Last sell price: " + str(price))
+            openOrders = (client.get_active_orders(tokenPair))
             balance = client.get_coin_balance(currency)
             balance = (float(balance['balanceStr']) + float(extCoinBalance))
-            currentTicker = (client.get_tick(tokenPair)['lastDealPrice'])
             buyAmount = determine_buy_amount(balance)
-            buyPrice = determine_initial_buy_price(currentTicker)
+            buyPrice = determine_initial_buy_price(price)
             logging.info("setting buy of " + str(buyAmount) + " at " + str(buyPrice))
             logging.info (client.create_buy_order(tokenPair, buyPrice, buyAmount))
             sellAmount = determine_sell_amount(balance)
-            sellPrice = determine_initial_sell_price(currentTicker)
+            sellPrice = determine_initial_sell_price(price)
             logging.info("setting sell of " + str(sellAmount) + " at " + str(sellPrice))
             logging.info (client.create_sell_order(tokenPair, sellPrice, sellAmount))
+        else:
+            logging.info("No orders present...setting to ticker price")
+            balance = client.get_coin_balance(currency)
+            balance = (float(balance['balanceStr']) + float(extCoinBalance))
+            buyAmount = determine_buy_amount(balance)
+            price = (client.get_tick(tokenPair)['lastDealPrice'])
+            buyPrice = determine_initial_buy_price(price)
+            print (buyPrice)
+            logging.info("setting buy of " + str(buyAmount) + " at " + str(buyPrice))
+            logging.info(client.create_buy_order(tokenPair, buyPrice, buyAmount))
+            sellAmount = determine_sell_amount(balance)
+            sellPrice = determine_initial_sell_price(price)
+            logging.info("setting sell of " + str(sellAmount) + " at " + str(sellPrice))
+            logging.info(client.create_sell_order(tokenPair, sellPrice, sellAmount))
     except:
         logging.info ("Shit went sideways...")
 
@@ -106,4 +163,4 @@ while True:
         gc.collect()
         count = 0
     logging.info("Waiting " + str(checkInterval) + " for next cycle...")
-    time.sleep(checkInterval)
+    time.sleep(int(checkInterval))
